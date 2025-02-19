1cc4c3227f96f5d977a5d7dcb8c1b08100525913
tania shafiee <taniashafiee78@gmail.com>
2025-02-19 14:36:35 +0330

    footer

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -2,12 +2,12 @@
 
 import { CustomButtonProps } from "../../types"
 
-export default function CustomButton({title,containerStyles,handleClick} : CustomButtonProps) {
+export default function CustomButton({title,containerStyles,handleClick,btnType} : CustomButtonProps) {
     return(
         <div className="">
             <button
+            type={btnType || "button"}
             disabled={false}
-            type={"button"}
             className={`custom-btn ${containerStyles}`}
             onClick={handleClick}
             >
